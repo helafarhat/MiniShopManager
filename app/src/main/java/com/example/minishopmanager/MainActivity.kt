bd92ff7b08b8d2b7fcacf290d57bfeca066f5e28
helafarhat <helafahrhat480@gmail.com>
2026-09-23 14:52:37 +0100

    TP1  Update

--- app/src/main/java/com/example/minishopmanager/MainActivity.kt
+++ app/src/main/java/com/example/minishopmanager/MainActivity.kt
@@ -1,47 +1,89 @@
 package com.example.minishopmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.minishopmanager.ui.theme.MiniShopManagerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        Log.d("LIFECYCLE", "onCreate")
+
         setContent {
             MiniShopManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(
+                    onNextClick = {
+                        Toast.makeText(
+                            this,
+                            "Bonjour Hella Farhat !",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        val intent = Intent(this, ProfileActivity::class.java)
+                        startActivity(intent)
+                    }
+                )
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "onDestroy")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "MiniShopManager")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiniShopManagerTheme {
-        Greeting("Android")
+        Button(
+            onClick = onNextClick
+        ) {
+            Text(text = "Suivant")
+        }
     }
 }
---
+++ app/src/main/java/com/example/minishopmanager/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.minishopmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ProfileActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
